d4e07ed19080ce24b483dcfb4217fe7bbca68681
Raksha <raksha@drupaltechie.com>
2024-07-04 15:18:31 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+// document.getElementById("myH1").textContent=`Hello`
+
+//variable
+// let age=25;
+// let price=10.99;
+// let gpa=2.1;
+// console.log(typeof gpa);
+// console.log()
+// let firstName="hey";
+// let email="Hey@gmail.com"
+// console.log(typeof firstName);
+// console.log(`your email is ${email}`)
+// console.log(`Your name is ${firstName}`);
+
+
+// let fullName = " HEY";
+// let age = 25;
+// let isStudent = false;
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`;
+// document.getElementById("p2").textContent = age;
+// document.getElementById("p3").textContent = isStudent;
+
+// // ARITHMATIC OPERATORS
+
+// let students = 30;
+
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 1;
+// students = students / 1;
+// students = students ** 1;
+
+// let extraStudents = students % 3
+
+// students += 1
+
+// students -= 1
+
+// students /= 1
+
+// students *= 1
+
+// students **= 1
+
+// students %= 1
+// students++
+// students--
+// console.log(students)
+
+let result = 1+2*3+4**2;
+console.log(result);
+
+
